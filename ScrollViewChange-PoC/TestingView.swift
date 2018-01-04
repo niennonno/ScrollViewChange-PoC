0bb6e2eef940318b3afbcd7ba355a12b77a314d3
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2018-01-04 19:19:00 +0530

    First Commit.

---
+++ ScrollViewChange-PoC/TestingView.swift
@@ -0,0 +1,22 @@
+//
+//  TestingView.swift
+//  ScrollViewChange-PoC
+//
+//  Created by Aditya Vikram Godawat on 1/4/18.
+//  Copyright © 2018 Aditya Vikram Godawat. All rights reserved.
+//
+
+import UIKit
+
+class TestingView: UIView {
+
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var greeViewHeightConstraint: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- ScrollViewChange-PoC/ViewController.swift
+++ ScrollViewChange-PoC/ViewController.swift
@@ -10,16 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var heightConstraint: NSLayoutConstraint!
+    
+    let aView = Bundle.main.loadNibNamed("TestingView", owner: self, options: nil)?.first as! TestingView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        aView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(aView)
+
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view": aView]))
+        
+        // align aView from the bottom
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view": aView]))
+        
+        // height constraint
+        heightConstraint = NSLayoutConstraint(item: aView, attribute: NSLayoutAttribute.height, relatedBy: .equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 200)
+        
+        self.aView.addConstraint(heightConstraint)
+        
+//        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[view(==200)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view": aView]))
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(scrollView.contentOffset.y)
+        if scrollView.contentOffset.y >= 0 && scrollView.contentOffset.y <= 160 {
+            heightConstraint.constant = 200 - scrollView.contentOffset.y
+            aView.setNeedsUpdateConstraints()
+        } else if scrollView.contentOffset.y > 16- {
+            heightConstraint.constant = 40
+            aView.setNeedsUpdateConstraints()
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+       
+        UIView.animate(withDuration: 0.6) {
+            self.aView.layoutIfNeeded()
+
+        }
+    }
+    
+//    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+//        print(scrollView.contentOffset.y)
+//        if scrollView.contentOffset.y >= 10 {
+//            heightConstraint.constant = 40
+//        } else {
+//            heightConstraint.constant = 200
+//        }
+//
+//        UIView.animate(withDuration: 1, animations: {
+//            self.view.setNeedsLayout()
+//        })
+//    }
+}
